76b76b6783d6581f86f923eba1ed2badea1f2423
Sacha <sacha.harel@gmail.com>
2024-12-18 11:43:18 +0100

    UPDATES // Commit before merge, userId updated on refresh

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,15 @@ import LoginForm from "./components/LoginForm";
 import RegisterForm from "./components/RegisterForm";
 import MessagesPage from "./components/Messages";
 
+import "./styles/App.css";
+import LeftMenu from "./components/LeftMenu";
+
 function App() {
   return (
     <Router>
+      <LeftMenu />
       <Routes>
+        <Route path="/" element={<LoginForm />} />
         <Route path="/login" element={<LoginForm />} />
         <Route path="/register" element={<RegisterForm />} />
         <Route path="/message/:userId" element={<MessagesPage />} />
