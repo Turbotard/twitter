4c5de581e1bc0fa8ec40e9ed9582ce2cad708af8
Sacha <sacha.harel@gmail.com>
2024-12-18 12:00:49 +0100

    UPDATES // Working persisting local storage

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,18 +1,24 @@
 import { create } from 'zustand';
+import { persist, PersistOptions } from 'zustand/middleware';
 
 type StoreState = {
   userId: string;
   removeUserId: () => void;
   updateUserId: (newUserId: string) => void;
-  getUserId: () => string;
 };
 
-const store = create<StoreState>((set) => ({
-  userId: "",
-  removeUserId: () => set({ userId: "" }),
-  updateUserId: (newUserId: string) => set({ userId: newUserId }),
-  getUserId: (): string => store.getState().userId,
-}));
+const store = create<StoreState>()(
+  persist(
+    (set) => ({
+      userId: "",
+      removeUserId: () => set({ userId: "" }),
+      updateUserId: (newUserId: string) => set({ userId: newUserId }),
+    }),
+    {
+      name: 'user-storage',
+    } as PersistOptions<StoreState>
+  )
+);
 
 export const useStore = store;
 
